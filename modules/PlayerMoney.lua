5179af469e9056b5f7576e0cad7e5814490d06fa
Adam Cunningham <57463819+okeanskiy@users.noreply.github.com>
2022-03-27 14:27:36 -0400

    Update PlayerMoney.lua

--- modules/PlayerMoney.lua
+++ modules/PlayerMoney.lua
@@ -3,6 +3,7 @@
 local CURRENCY_NAME = "Money"
 local DEFAULT_AMOUNT = 100
 local LEADERSTATS_ENABLED = true
+local NEGATIVE_MONEY = false
 
 -- game services
 local Players = game:GetService("Players")
@@ -14,76 +15,79 @@ local PlayerMoney = {}
 PlayerMoney.Values = {}
 
 local function updateLeaderstats(player)
-    player.leaderstats[CURRENCY_NAME].Value = PlayerMoney.Values[player]
+	player.leaderstats[CURRENCY_NAME].Value = PlayerMoney.Values[player]
 end
 
 -- Get player's current money
 -- (Player player) -> number money
 function PlayerMoney.Get(player)
-    return PlayerMoney.Values[player]
+	return PlayerMoney.Values[player]
 end
 
 -- Add (or subtract with negative number) money to a player
 -- (Player player, number amount) -> void
 function PlayerMoney.Increment(player, amount)
-    if PlayerMoney.Values[player] == nil then return end
-    PlayerMoney.Values[player] += amount
-    if LEADERSTATS_ENABLED then
-        updateLeaderstats(player)
-    end
+	if PlayerMoney.Values[player] == nil then return end
+	PlayerMoney.Values[player] += amount
+	if PlayerMoney.Values[player] < 0 and not NEGATIVE_MONEY then
+		PlayerMoney.Values[player] = 0
+	end
+	if LEADERSTATS_ENABLED then
+		updateLeaderstats(player)
+	end
 end
 
 -- Set player's money to a specific value
 -- (Player player, number setTo) -> void
 function PlayerMoney.Set(player, setTo)
-    if PlayerMoney.Values[player] == nil then return end
-    PlayerMoney.Values[player] = setTo
-    if LEADERSTATS_ENABLED then
-        updateLeaderstats(player)
-    end
+	if PlayerMoney.Values[player] == nil then return end
+	PlayerMoney.Values[player] = if NEGATIVE_MONEY then setTo else math.max(0, setTo)
+	if LEADERSTATS_ENABLED then
+		updateLeaderstats(player)
+	end
 end
 
 local function addLeaderstats(player)
-    local leaderstats = player:FindFirstChild("leaderstats")
-    if not leaderstats then
-        leaderstats = Instance.new("Folder")
-        leaderstats.Name = "leaderstats"
-        leaderstats.Parent = player
-    end
-    local money = leaderstats:FindFirstChild(CURRENCY_NAME)
-    if not money then
-        money = Instance.new("NumberValue")
-        money.Name = CURRENCY_NAME
-        money.Parent = leaderstats
-    end
+	local leaderstats = player:FindFirstChild("leaderstats")
+	if not leaderstats then
+		leaderstats = Instance.new("Folder")
+		leaderstats.Name = "leaderstats"
+		leaderstats.Parent = player
+	end
+	local money = leaderstats:FindFirstChild(CURRENCY_NAME)
+	if not money then
+		money = Instance.new("NumberValue")
+		money.Name = CURRENCY_NAME
+		money.Parent = leaderstats
+	end
 end
 
 local function onPlayerAdded(player)
-    -- replace 'DEFAULT_VALUE' with a datastore implementation to load saved money
-    PlayerMoney.Values[player] = DEFAULT_VALUE
-    
-    -- player may have left during datastore get
-    -- if not player:IsDescendantOf(Players) then return end
-    
-    if LEADERSTATS_ENABLED then
-        addLeaderstats(player)
-        updateLeaderstats(player)
-    end
+	-- replace 'DEFAULT_VALUE' with a datastore implementation to load saved money
+	PlayerMoney.Values[player] = DEFAULT_AMOUNT
+
+	-- player may have left during datastore get
+	-- if not player:IsDescendantOf(Players) then return end
+
+	if LEADERSTATS_ENABLED then
+		addLeaderstats(player)
+		updateLeaderstats(player)
+	end
 end
 
 local function onPlayerRemoving(player)
-    -- datasave here
-    PlayerMoney.Values[player] = nil
+	-- datasave here
+	PlayerMoney.Values[player] = nil
 end
 
 local function init()
-    Players.PlayerAdded:Connect(onPlayerAdded)
-    for _, player in ipairs(Players:GetPlayers()) do
-        onPlayerAdded(player)
-    end
-    Players.PlayerRemoving:Connect(onPlayerRemoving)
+	Players.PlayerAdded:Connect(onPlayerAdded)
+	for _, player in ipairs(Players:GetPlayers()) do
+		onPlayerAdded(player)
+	end
+	Players.PlayerRemoving:Connect(onPlayerRemoving)
 end
 
 init()
-        
+
 return PlayerMoney
